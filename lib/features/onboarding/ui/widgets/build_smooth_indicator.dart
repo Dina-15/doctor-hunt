0967c0712719cf2f8ed67430705311914e97edf0
Dina Nasser <d.dinanasser2004@gmail.com>
2024-10-10 21:44:24 +0300

    create onboarding view

---
+++ lib/features/onboarding/ui/widgets/build_smooth_indicator.dart
@@ -0,0 +1,25 @@
+import 'package:doctor_hunt/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BuildSmoothIndicator extends StatelessWidget {
+  const BuildSmoothIndicator(
+      {super.key, required this.index, required this.currPageIndex});
+  final int index;
+  final int currPageIndex;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(microseconds: 250),
+      width: 10.w,
+      height: 10.h,
+      margin: const EdgeInsets.symmetric(horizontal: 3),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: index == currPageIndex
+            ? ColorsManager.primaryColor
+            : Colors.grey[350],
+      ),
+    );
+  }
+}
