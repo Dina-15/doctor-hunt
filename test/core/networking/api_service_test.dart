b998974e525839e59df96c4d9e70060c1230c2f2
axah710 <ahmed36085361@gmail.com>
2024-11-17 11:06:05 +0200

    Refactor API Service Tests and Add Constants for Request Bodies
    - Introduced constants for email, wrongEmail, password, wrongPassword, name, gender, and phone to avoid repetition and improve readability in test cases.
    - Updated test cases to use these constants for request bodies in login and sign-up methods.
    - Corrected the success message in the expected response for the sign-up method from "Loggedin Successfuly." to "Signed up Successfully."
    - Added a skip flag to the sign-up failure test to temporarily ignore it, with a comment explaining its purpose.

--- test/core/networking/api_service_test.dart
+++ test/core/networking/api_service_test.dart
@@ -8,25 +8,31 @@ import 'package:doctor_hunt/features/sign_up/data/models/sign_up_response.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
 import 'api_service_test.mocks.dart';
 
 @GenerateMocks([ApiService])
 void main() {
   late MockApiService mockApiService;
 
+  const String email = 'a@gmail.com';
+  const String wrongEmail = 'wrong@gmail.com';
+  const String password = 'password';
+  const String wrongPassword = 'wrong-password';
+  const String name = "user";
+  const int gender = 0;
+  const String phone = "0122";
+
   setUp(() {
     mockApiService = MockApiService();
   });
+
   group('Login API Tests', () {
     test('login method should return success response', () async {
       // arrange
       final loginRequestBody =
-          LoginRequestBody(email: 'a@gmail.com', password: 'password');
-
+          LoginRequestBody(email: email, password: password);
       final expectedLoginResponse = LoginResponse(
-          message: "Loggedin Successfuly.", code: 200, status: true);
-
+          message: "Logged in Successfully.", code: 200, status: true);
       when(mockApiService.login(loginRequestBody))
           .thenAnswer((_) => Future.value(expectedLoginResponse));
 
@@ -39,9 +45,8 @@ void main() {
 
     test('login method should handle failure response', () async {
       // arrange
-      final loginRequestBody = LoginRequestBody(
-          email: 'wrong@gmail.com', password: 'wrong-password');
-
+      final loginRequestBody =
+          LoginRequestBody(email: wrongEmail, password: wrongPassword);
       when(mockApiService.login(loginRequestBody)).thenThrow(
         DioException(
           requestOptions: RequestOptions(
@@ -60,16 +65,14 @@ void main() {
     test('sign up method should return success response', () async {
       // arrange
       final signUpRequestBody = SignUpRequestBody(
-          email: 'a@gmail.com',
-          name: "user",
-          gender: 0,
-          password: 'password',
-          passwordConfirmation: "password",
-          phone: "0122");
-
+          email: email,
+          name: name,
+          gender: gender,
+          password: password,
+          passwordConfirmation: password,
+          phone: phone);
       final expectedSignUpResponse = SignUpResponse(
-          message: "Loggedin Successfuly.", code: 200, status: true);
-
+          message: "Signed up Successfully.", code: 200, status: true);
       when(mockApiService.signUp(signUpRequestBody))
           .thenAnswer((_) => Future.value(expectedSignUpResponse));
 
@@ -83,13 +86,12 @@ void main() {
     test('sign up method should handle failure response', () async {
       // arrange
       final signUpRequestBody = SignUpRequestBody(
-          email: 'wrong@gmail.com',
-          name: "user",
-          gender: 0,
-          password: 'wrong-password',
-          passwordConfirmation: "wrong-password",
-          phone: "0122");
-
+          email: wrongEmail,
+          name: name,
+          gender: gender,
+          password: wrongPassword,
+          passwordConfirmation: wrongPassword,
+          phone: phone);
       when(mockApiService.signUp(signUpRequestBody)).thenThrow(
         DioException(
           requestOptions: RequestOptions(
@@ -99,8 +101,13 @@ void main() {
       );
 
       // act & assert
-      await expectLater(() async => mockApiService.signUp(signUpRequestBody),
-          throwsA(isA<DioException>()));
+      await expectLater(
+        () async => mockApiService.signUp(signUpRequestBody),
+        throwsA(isA<DioException>()),
+        //! Use Skip in Dart to ignore tests for incomplete features or temporary issues.
+        //! "I added the 'skip' to inform you about its functionality. Feel free to comment it, or remove it if necessary." ...
+        skip: 'This test is currently not applicable',
+      );
     });
   });
 }
